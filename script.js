0597b0a6fef79399857f33471ed5883e912ac6e6
Vlad Cervac <cervacvad32@gmail.com>
2023-04-19 19:22:01 +0200

    added functonality to all btn

--- script.js
+++ script.js
@@ -5,59 +5,147 @@ let btnAccelerare = document.querySelector('.accelerare'),
 	anFabricare = document.querySelector('.anFabricare'),
 	selectMasina = document.querySelector('.selectMasina'),
 	descriereMasine = document.querySelector('.descriereMasineiSelectate'),
-	afiseazaMasinaSelectata = document.querySelector('.afiseazamasinaSelectata'),
-	afiseazaMasinaInfoDescrisa = document.querySelector('.afiseazaInformatiiMasinaDorita');
+	afiseazaMasinaSelectata = document.querySelector('.card-masina-selectata'),
+	afiseazaMasinaInfoDescrisa = document.querySelector('.afiseazaInformatiiMasinaDorita'),
+	btnIncarcare = document.querySelector('.incarcare'),
+	btnRadioElectric = document.querySelector('#electric'),
+	btnRadioFuel = document.querySelector('#fuel'),
+	baterie = document.querySelector('#baterie'),
+	vitezaMaxima = document.querySelector('#viteza-maxima'),
+	descriereMasina = document.querySelector('.card-masina-selectata'),
+	textAcelereaza = document.querySelector('.accelereaza')
+	
 
 class Masina {
 	constructor(model, culoare, anFabricare, vitezaMaxima){
 		this.model = model;
 		this.culoare = culoare;
 		this.anFabricare = anFabricare;
-		this.vitezaInitiala = 0;
+		this.viteza = 0;
 		this.vitezaMaxima = vitezaMaxima;
 	}
+	
 	accelereaza(){
-		return this.vitezaInitiala += 10;
+		this.viteza += 10;
 	}
 	frineaza() {
-		return this.vitezaInitiala -10;
+		this.viteza -= 10;
 	}
 	descriere() {
 		return `${this.model} ${this.culoare}, fabricat in ${this.anFabricare} merge cu ${this.vitezaMaxima} km/h`
 	}
 }
+class MasinaElectrica extends Masina {
+	constructor(model, culoare, anFabricare, vitezaMaxima, baterie) {
+		super(model, culoare, anFabricare, vitezaMaxima)
+		this.baterie = baterie;
+		this.viteza = 0	
+	}
+	accelereaza() {
+		this.viteza += 10;
+		this.baterie -= 5
+	}
+	frineaza() {
+		this.viteza -=10;
+	}
+	incarcare() {
+		this.baterie >= 100 ? this.baterie = 100 : this.baterie +=10	
+	}
+	descriere() {
+		return `${this.model} ${this.culoare}, fabricat in ${this.anFabricare} merge cu ${this.vitezaMaxima} km/h si are ${this.baterie}% baterie`
+	}
 
-let ford = new Masina('Ford focus', 'rosu', 2019, 220);
-let bmw = new Masina ('Bmw M5', 'neagru', 2023, 320);
-let kia = new Masina ('Kia Ceed', 'albastru', 2020, 220);
-let mercedes = new Masina ('Mercedes AMG', 'alb', 2023, 320);
-
-afiseazaMasinaSelectata.addEventListener('click', function () {
-	descriereMasine.textContent = `${selectMasina.value}.{descriere()`;
-	
-	if (selectMasina.value == 'ford') {
-		textcontent = ford.descriere()
-		descriereMasine.textContent = textcontent;
-	} else if (selectMasina.value == 'bmw') {
-		textcontent = bmw.descriere()
-		descriereMasine.textContent = textcontent;
-	} else if (selectMasina.value == 'kia') {
-		textcontent = kia.descriere()
-		descriereMasine.textContent = textcontent;
+}
+let masinaElectrica = '';
+let masina = '';
+afiseazaMasinaInfoDescrisa.addEventListener('click', function(){
+	if (btnRadioElectric.checked) {
+		masinaElectrica = new MasinaElectrica(model.value, culoare.value, anFabricare.value, vitezaMaxima.value, Number(baterie.value))
+		descriereMasina.textContent = masinaElectrica.descriere();
 	} else {
-		textcontent = mercedes.descriere()
-		
+		masina = new Masina(model.value, culoare.value, anFabricare.value, vitezaMaxima.value)
+		descriereMasina.textContent = masina.descriere();
 	}
 })
-
-afiseazaMasinaInfoDescrisa.addEventListener('click', function(){
-	let masina = new Masina(model.value, culoare.value, anFabricare.value, 220);
-	descriereMasine.textContent = `${masina.descriere()}`;
+btnAccelerare.addEventListener('click', function() {
+	if (btnRadioElectric.checked) {
+		masinaElectrica.accelereaza();
+		textAcelereaza.textContent = `La moment masina accelereaza cu ${masinaElectrica.viteza}km/h, baterie: ${masinaElectrica.baterie}%`
+	} else {
+		masina.accelereaza();
+		textAcelereaza.textContent = `La moment masina accelereaza cu ${masina.viteza}km/h`;
+	}
 })
-btnAccelerare.addEventListener('click', function(){
-	let textViteza = document.querySelector('.viteza');
-	let masina = new Masina(model.value, culoare.value, anFabricare.value, 220);
-	textViteza.textContent = `${masina.accelereaza()}`;
+btnFrinare.addEventListener('click', function(){
+	if (btnRadioElectric.checked) {
+		masinaElectrica.frineaza();
+		textAcelereaza.textContent = `La moment masina accelereaza cu ${masinaElectrica.viteza}km/h, baterie: ${masinaElectrica.baterie}%`
+	} else {
+		masina.frineaza();
+		textAcelereaza.textContent = `La moment masina accelereaza cu ${masina.viteza}km/h`;
+	}
+})
+btnIncarcare.addEventListener('click', function() {
+	if (btnRadioElectric.checked) {
+		masinaElectrica.incarcare();
+		textAcelereaza.textContent = `La moment masina accelereaza cu ${masinaElectrica.viteza}km/h, baterie: ${masinaElectrica.baterie}%`
+	} 
 })
 
 
+
+
+let carData = ''
+fetch('https://raw.githubusercontent.com/itemsapi/itemsapi-example-data/master/items/cars.json')
+.then(response => response.json())
+.then(data => {
+  const car1 = {data};
+  
+  car1.data.forEach(car => {
+	  const option = document.createElement('option');
+	  option.value = car.title
+	  option.text = car.title
+	  selectMasina.add(option);
+	   carData = car
+  });
+  afiseazaMasinaSelectata.addEventListener('click', function() {
+	console.log('click');
+	 for(let i = 0; i < car1.data.length; i++){
+		let  datOfCar = car1.data[i];
+		if (datOfCar.title == selectMasina.value) {
+			console.log(datOfCar);
+		}
+	
+	}
+	
+	
+	
+
+});
+})
+.catch(error => {
+  console.error('Eroare:', error);
+});
+const modelCar = 'camry';
+const url = 'https://api.api-ninjas.com/v1/cars?model=' + modelCar;
+const headers = {
+  'X-Api-Key': 'M6UECGdbZUGWjWxiDZc7ug==cdEBb8Yb0FFOBoiZ'
+};
+fetch(url, { headers })
+  .then(response => {
+	if (response.ok) {
+	  return response.json();
+	} else {
+	  throw new Error('Error: ' + response.status);
+	}
+  })
+  .then(data => {
+	console.log(data);
+  })
+  .catch(error => {
+	console.error(error);
+  });
+
+	
+
+	  
